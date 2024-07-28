6fcdea9d567e200055a8e90ba9bed0b313624d6f
jaspherG <guitierrezapey@gmail.com>
2024-07-29 01:41:03 +0800

    aro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,20 +45,7 @@
     <!--   Core JS Files   -->
   <script src="{{ url('assets/js/core/popper.min.js') }}"></script>
   <script src="{{ asset('assets/js/core/bootstrap.min.js') }}"></script>
-  <!-- <script src="{{ url('assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
-  <script src="{{ url('assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
-  <script src="{{ url('assets/js/plugins/fullcalendar.min.js') }}"></script>
-  <script src="{{ url('assets/js/plugins/chartjs.min.js') }}"></script> -->
   @stack('dashboard')
-  <!-- <script>
-    var win = navigator.platform.indexOf('Win') > -1;
-    if (win && document.querySelector('#sidenav-scrollbar')) {
-      var options = {
-        damping: '0.5'
-      }
-      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-    }
-  </script> -->
 
   <!-- Github buttons -->
   <script async defer src="https://buttons.github.io/buttons.js') }}"></script>
