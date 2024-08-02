d57a07b3716465ad4618aed04d4542f52629a5d2
jaspherG <guitierrezapey@gmail.com>
2024-08-02 08:51:01 +0800

    guest

--- resources/views/layouts/user_type/guest.blade.php
+++ resources/views/layouts/user_type/guest.blade.php
@@ -2,7 +2,7 @@
 
 @section('guest')
     @if(\Request::is('login/forgot-password')) 
-        @include('layouts.navbars.auth.sidebar')
+    @include('layouts.navbars.guest.nav')
         @yield('content') 
     @else
         <div class="container position-sticky z-index-sticky top-0">
