962a30913fd80c11baf95bafa9f9e285be91b988
jaspherG <guitierrezapey@gmail.com>
2024-08-06 14:15:17 +0800

    arotrack

--- resources/views/session/login-session.blade.php
+++ resources/views/session/login-session.blade.php
@@ -2,51 +2,59 @@
 
 @section('content')
 
-<main class="main-content mt-0">
+<main class="main-content  mt-0">
+<section>
+<main class="main-content  mt-0">
     <section>
-        <div class="page-header min-vh-75">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
-                        <div class="card card-plain mt-8">
-                            <div class="card-header pb-0 text-left bg-transparent">
-                                <h3 class="font-weight-bolder text-dark text-gradient">AROTrack: Documents Tracking and Monitoring System</h3>
-                            </div>
-
-                            <div class="card-body">
-                                <div class="mb-3">
+      <div class="page-header min-vh-75">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
+              <div class="card card-plain mt-8">
+                <div class="card-header pb-0 text-left bg-transparent">
+                <h3 class="font-weight-bolder text-dark text-gradient">AROTrack: Documents Tracking and Monitoring System</h3>
+                </div>
+                <div class="card-body">
+                <div class="mb-3">
                                     <p id="error-message" class="text-danger mt-2"></p>
                                     <p id="attempt-message" class="text-danger mt-2"></p>
                                     <div id="timer" style="display:none;">
                                         Too many attempts. Please try again in <span id="countdown"></span>
                                     </div>
                                 </div>
-
-                                <form id="loginForm">
-                                    @csrf
-                                    <label>Username</label>
-                                    <div class="mb-3">
-                                        <input required type="email" class="form-control" name="email" id="email" placeholder="Email" value="" aria-label="Email" aria-describedby="email-addon">
-                                    </div>
-                                    <label>Password</label>
-                                    <div class="mb-3">
-                                        <input required type="password" class="form-control" name="password" id="password" placeholder="Password" value="" aria-label="Password" aria-describedby="password-addon">
-                                    </div>
-                                    <div class="">
-                                        <span id="togglePasswordIcon" class="toggle-password" onclick="togglePassword()">Show Password</span>
-                                    </div>
-                                    <div class="text-center">
-                                        <button type="submit" id="submit" class="btn bg-danger w-100 mt-4 mb-0 text-white">Login</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
+                  <form id="loginForm" role="form" method="POST" action="/session">
+                    @csrf
+                    <label>Email</label>
+                    <div class="mb-3">
+                      <input type="email" class="form-control" name="email" id="email" placeholder="Email" value="" aria-label="Email" aria-describedby="email-addon">
+                      
                     </div>
+                    <label>Password</label>
+                    <div class="mb-3">
+                      <input required type="password" class="form-control" name="password" id="password" placeholder="Password" value="" aria-label="Password" aria-describedby="password-addon">
+                    </div>
+
+                    <div class="">
+                        <span id="togglePasswordIcon" class="toggle-password" onclick="togglePassword()">Show Password</span>
+                    </div>
+
+                    <div class="text-center">
+                      <button type="submit" id="submit" class="btn bg-danger w-100 mt-4 mb-0 text-white">Sign in</button>
+                    </div>
+                  </form>
                 </div>
+              </div>
             </div>
+            <div class="col-md-6">
+              <div class="oblique position-absolute top- h-100 d-md-block d-none me-n8">
+                <div class="oblique-image bg-cover position-absolute fixed-top ms-auto h-100 z-index-0 ms-n6" style="background-image:url('../assets/img/pup.jpg')"></div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </section>
-</main>
+  </main>
 
 <!-- Forget Password Modal -->
 <div class="modal fade" id="forgetPasswordModal" tabindex="-1" aria-labelledby="forgetPasswordModalLabel" aria-hidden="true">
